131faa5e959c8480b02eb67ae172583e24555eac
lamin you <lmin5514@gmail.com>
2026-08-01 23:42:34 +0630

    fix(theme): include vuetify color definitions for production build

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import '@mdi/font/css/materialdesignicons.css'
 
 // Vuetify imports
 import 'vuetify/styles'           // global styles
-import 'vuetify/styles/main.css'
+import 'vuetify/lib/styles/main.sass'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
@@ -22,17 +22,16 @@ const vuetify = createVuetify({
     },
     theme: {
         defaultTheme: 'light',
-        theme: {
-            defaultTheme: 'light',
-            themes: {
-                light: {
-                    colors: {
-                        primary: colors.green.darken4, // #1B5E20
-                        secondary: colors.green.lighten5,
-                    }
-                }
-            }
-        }
+        themes: {
+            light: {
+                colors: {
+                    // Custom green colors ကို Vuetify color Object မှ ညွှန်းပေးထားခြင်း
+                    'green-darken-4': colors.green.darken4,
+                    'green-lighten-5': colors.green.lighten5,
+                    'green-lighten-4': colors.green.lighten4,
+                },
+            },
+        },
     },
 })
 
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,11 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  pluginOptions: {
+    vuetify: {
+      styles: 'expose' 
+    }
+  },
   publicPath: '/',
   devServer: {
     client: {
